5cc082f10835353d8dca191b2cc8aadce7526e89
xushanning <474003561@qq.com>
2020-06-19 20:03:51 +0800

    .

--- common_lib/src/main/java/com/xu/commonlib/utlis/extention/RxJavaExtend.kt
+++ common_lib/src/main/java/com/xu/commonlib/utlis/extention/RxJavaExtend.kt
@@ -3,6 +3,8 @@ package com.xu.commonlib.utlis.extention
 import com.xu.commonlib.mvp.IView
 import com.xu.commonlib.utlis.TransformUtil
 import io.reactivex.Flowable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 
 /**
  * @author 言吾許
@@ -15,4 +17,11 @@ import io.reactivex.Flowable
 fun <T> Flowable<T>.flowableTransform(mView: IView): Flowable<T> {
     return this.compose(TransformUtil.defaultFlowableSchedulers())
         .compose(mView.bindToLife())
+}
+
+/**
+ *添加 disposable到CompositeDisposable中去
+ */
+fun Disposable.addTo(compositeDisposable: CompositeDisposable) {
+    compositeDisposable.add(this)
 }
