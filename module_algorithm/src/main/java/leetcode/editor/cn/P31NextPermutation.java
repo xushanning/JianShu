8a627f7144b0dd679868adf7c5c6e3457a9d1e15
xushanning <shanningxu@163.com>
2020-07-09 00:15:43 +0800

    .

--- module_algorithm/src/main/java/leetcode/editor/cn/P31NextPermutation.java
+++ module_algorithm/src/main/java/leetcode/editor/cn/P31NextPermutation.java
@@ -19,15 +19,61 @@ public class P31NextPermutation {
     public static void main(String[] args) {
         Solution solution = new P31NextPermutation().new Solution();
         // TO TEST
+        //solution.nextPermutation(new int[]{1, 2, 3, 8, 5, 7, 6, 4});
+        solution.nextPermutation(new int[]{1, 2, 3});
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public void nextPermutation(int[] nums) {
+            int len = nums.length;
+            if (len == 0) {
+                return;
+            }
+
 
             //12385764->12386457
 
+            //1、从后向前找到第一个对升序的元素(i,j)
+            int i = len - 2;
+            while (i >= 0 && nums[i + 1] <= nums[i]) {
+                i--;
+            }
+            //2、在[j,end)中找到第一个比nums[i]大的数字nums[k]
+            if (i >= 0) {
+                int j = i + 1;
+                int k = j+1;
+                while (k < len) {
+                    if (nums[k] > nums[i]) {
+                        break;
+                    }
+                    k++;
+                }
+
+                //3、交换i和k的值
+                swap(nums, i, k);
+                //4、从小到大排序(j,end)
+                reverse(nums, j);
+            }
+
+
         }
+
+        private void reverse(int[] nums, int start) {
+            int i = start, j = nums.length - 1;
+            while (i < j) {
+                swap(nums, i, j);
+                i++;
+                j--;
+            }
+        }
+
+        private void swap(int[] nums, int i, int j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
---
+++ module_algorithm/src/main/java/leetcode/editor/cn/P49GroupAnagrams.java
@@ -0,0 +1,61 @@
+//给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。 
+//
+// 示例: 
+//
+// 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+//输出:
+//[
+//  ["ate","eat","tea"],
+//  ["nat","tan"],
+//  ["bat"]
+//] 
+//
+// 说明： 
+//
+// 
+// 所有输入均为小写字母。 
+// 不考虑答案输出的顺序。 
+// 
+// Related Topics 哈希表 字符串 
+// 👍 386 👎 0
+
+
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//Java：字母异位词分组
+public class P49GroupAnagrams {
+    public static void main(String[] args) {
+        Solution solution = new P49GroupAnagrams().new Solution();
+        // TO TEST
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<List<String>> groupAnagrams(String[] strs) {
+            int len = strs.length;
+            if (len == 0) {
+                return new ArrayList<>();
+            }
+            Map<String, List<String>> map = new HashMap<>();
+            for (int i = 0; i < len; i++) {
+                char[] c = strs[i].toCharArray();
+                Arrays.sort(c);
+                String s = String.valueOf(c);
+                if (!map.containsKey(s)) {
+                    map.put(s, new ArrayList<>());
+                }
+                map.get(s).add(strs[i]);
+            }
+            return new ArrayList<>(map.values());
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
