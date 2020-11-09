7f14f52332b89e95899a022d44ef6a1a1f11c242
xushanning <474003561@qq.com>
2020-11-09 20:55:18 +0800

    .

---
+++ module_algorithm/src/main/java/leetcode/editor/cn/P143ReorderList.java
@@ -0,0 +1,69 @@
+//给定一个单链表 L：L0→L1→…→Ln-1→Ln ， 
+//将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→… 
+//
+// 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。 
+//
+// 示例 1: 
+//
+// 给定链表 1->2->3->4, 重新排列为 1->4->2->3. 
+//
+// 示例 2: 
+//
+// 给定链表 1->2->3->4->5, 重新排列为 1->5->2->4->3. 
+// Related Topics 链表 
+// 👍 450 👎 0
+
+
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Java：重排链表
+public class P143ReorderList {
+    public static void main(String[] args) {
+        Solution solution = new P143ReorderList().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public void reorderList(ListNode head) {
+            if (head == null) {
+                return;
+            }
+            List<ListNode> list = new ArrayList<>();
+            while (head != null) {
+                list.add(head);
+                head = head.next;
+            }
+            int start = 0;
+            int end = list.size() - 1;
+            //给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
+            //将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→…
+            while (start < end) {
+                list.get(start).next = list.get(end);
+                start++;
+                if (start == end) {
+                    break;
+                }
+                list.get(end).next = list.get(start);
+                end--;
+            }
+            list.get(start).next = null;
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
