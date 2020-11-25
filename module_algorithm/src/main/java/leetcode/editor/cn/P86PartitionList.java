ff731dfdf00cc347df9ced3d818a989686f27d85
xushanning <474003561@qq.com>
2020-11-25 21:00:02 +0800

    .

---
+++ module_algorithm/src/main/java/leetcode/editor/cn/P86PartitionList.java
@@ -0,0 +1,62 @@
+//给定一个链表和一个特定值 x，对链表进行分隔，使得所有小于 x 的节点都在大于或等于 x 的节点之前。 
+//
+// 你应当保留两个分区中每个节点的初始相对位置。 
+//
+// 
+//
+// 示例: 
+//
+// 输入: head = 1->4->3->2->5->2, x = 3
+//输出: 1->2->2->4->3->5
+// 
+// Related Topics 链表 双指针 
+// 👍 279 👎 0
+
+
+package leetcode.editor.cn;
+
+//Java：分隔链表
+public class P86PartitionList {
+    public static void main(String[] args) {
+        Solution solution = new P86PartitionList().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public ListNode partition(ListNode head, int x) {
+            if (head == null) {
+                return null;
+            }
+            ListNode leftDummy = new ListNode(0);
+            ListNode rightDummy = new ListNode(0);
+            ListNode leftCur = leftDummy;
+            ListNode rightCur = rightDummy;
+            ListNode cur = head;
+            while (cur != null) {
+                if (cur.val < x) {
+                    leftCur.next = cur;
+                    leftCur = leftCur.next;
+                } else {
+                    rightCur.next = cur;
+                    rightCur = rightCur.next;
+                }
+                cur = cur.next;
+            }
+            leftCur.next = rightDummy.next;
+            //忘了这一行了
+            rightCur.next = null;
+            return leftDummy.next;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
