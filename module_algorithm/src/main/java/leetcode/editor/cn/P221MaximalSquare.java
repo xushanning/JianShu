1aea1aa6c459b6b719595aaccb76d2755a06e758
xushanning <474003561@qq.com>
2020-11-26 20:16:59 +0800

    .

---
+++ module_algorithm/src/main/java/leetcode/editor/cn/P221MaximalSquare.java
@@ -0,0 +1,56 @@
+//在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。 
+//
+// 
+//
+// 示例： 
+//
+// 
+//输入：
+//matrix = [["1","0","1","0","0"],
+//          ["1","0","1","1","1"],
+//          ["1","1","1","1","1"],
+//          ["1","0","0","1","0"]]
+//
+//输出：4 
+// Related Topics 动态规划 
+// 👍 622 👎 0
+
+
+package leetcode.editor.cn;
+
+//Java：最大正方形
+public class P221MaximalSquare {
+    public static void main(String[] args) {
+        Solution solution = new P221MaximalSquare().new Solution();
+        // TO TEST
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int maximalSquare(char[][] matrix) {
+            int m = matrix.length;
+            if (m == 0 || matrix[0].length == 0) {
+                return 0;
+            }
+            int n = matrix[0].length;
+            //dp[i][j]是i j位置结尾的最长边长
+            int[][] dp = new int[m][n];
+            int max = 0;
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (matrix[i][j] == '1') {
+                        if (i == 0 || j == 0) {
+                            dp[i][j] = 1;
+                        } else {
+                            dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                        }
+                        max = Math.max(max, dp[i][j]);
+                    }
+                }
+            }
+            return max * max;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
