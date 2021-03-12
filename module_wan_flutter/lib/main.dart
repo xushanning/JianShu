f28c1660a31c174f49f34f2fafa1122f3162e58c
xushanning <shanningxu@163.com>
2021-03-12 22:28:25 +0800

    .

--- module_wan_flutter/lib/main.dart
+++ module_wan_flutter/lib/main.dart
@@ -3,6 +3,8 @@ import 'package:module_wan_flutter/routes/route.dart';
 
 ///代码千万行，注释第一行，代码不规范，同事两行泪
 void main() => runApp(new MaterialApp(
+      //去掉debug图标
+      debugShowCheckedModeBanner: false,
       title: 'title',
       theme: ThemeData(primarySwatch: Colors.blue),
       initialRoute: '/main',
