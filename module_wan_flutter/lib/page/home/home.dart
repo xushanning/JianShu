ab34cd1be073428056dc0c8c96aafd083a829b4a
xushanning <shanningxu@163.com>
2021-03-18 00:52:52 +0800

    .

--- module_wan_flutter/lib/page/home/home.dart
+++ module_wan_flutter/lib/page/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:module_wan_flutter/bean/home_item_bean.dart';
 import 'package:provider/provider.dart';
 
 import 'home_model.dart';
@@ -35,14 +36,57 @@ class HomePage extends StatelessWidget {
                           )),
                 Expanded(
                   child: ListView.builder(
-                      itemCount: 50,
+                      itemCount: model.itemData.length,
                       itemBuilder: (context, index) {
-                        return Text(model.itemData[index].title);
+                        return _getItem(model.itemData[index]);
                       }),
                 )
               ],
             ),
           );
         });
+  }
+
+  Card _getItem(HomeItemBean bean) {
+    return Card(
+      margin: EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
+      elevation: 3,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(bean.author),
+              _getRedFrameText("置顶"),
+              _getRedFrameText("新"),
+              //todo 是否还有其他方式中间撑开？
+              Expanded(child: SizedBox()),
+              Text(
+                bean.publishTime.toString(),
+              )
+            ],
+          ),
+          Text(
+            bean.title,
+            textAlign: TextAlign.left,
+          )
+        ],
+      ),
+    );
+  }
+
+  ///带红框框的Text
+  Container _getRedFrameText(String content) {
+    return Container(
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(2)),
+          //背景颜色
+          border: Border.all(width: 1, color: Color(0xffF85959))),
+      child: Text(
+        content,
+        style: TextStyle(color: Color(0xffF85959)),
+      ),
+    );
   }
 }
